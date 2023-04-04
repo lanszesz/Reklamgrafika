f40474f84bce61f8d6f01e5f6a59dce5d1d8357d
erwin <58359015+lanszesz@users.noreply.github.com>
2023-04-04 22:29:01 +0300

    Footer

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
     <svg-component></svg-component>
     <div class="bg-ma-bg-01 py-[40px] px-[20px] flex flex-col gap-4 items-center justify-center text-center">
       <h3>A Madéfalvi Gyümölcs</h3>
-      <div class="flex gap-10 items-center">
-        <div class="w-[264px] max-h-[4px] p-[2px] bg-ma-dark"></div>
+      <div class="flex gap-10 items-center w-full">
+        <div class="w-[264px] max-h-[4px] p-[2px] bg-ma-dark rounded-[10px]"></div>
         <svg class="w-[150px] h-[70px] text-ma-red">
           <use xlink:href="#leaves-1"></use>
         </svg>
         <svg class="w-[150px] h-[70px] text-ma-red">
           <use xlink:href="#leaves-2"></use>
         </svg>
-        <div class="w-[264px] max-h-[4px] p-[2px] bg-ma-dark"></div>
+        <div class="w-[264px] max-h-[4px] p-[2px] bg-ma-dark rounded-[10px]"></div>
       </div>
       <p class="font-bold leading-[25px] text-[22px] text-ma-dark tracking-[1.1px]">{{ $t('footer.text') }}</p>
     </div>
