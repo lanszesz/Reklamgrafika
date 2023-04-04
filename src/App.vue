07a196fbecb689a6b21a8f73488be4ddefe4a0bb
erwin <58359015+lanszesz@users.noreply.github.com>
2023-04-04 22:30:52 +0300

    Footer

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <svg-component></svg-component>
     <div class="bg-ma-bg-01 py-[40px] px-[20px] flex flex-col gap-4 items-center justify-center text-center">
       <h3>A Madéfalvi Gyümölcs</h3>
-      <div class="flex gap-10 items-center w-full">
+      <div class="flex gap-10 items-center justify-center w-full">
         <div class="w-[264px] max-h-[4px] p-[2px] bg-ma-dark rounded-[10px]"></div>
         <svg class="w-[150px] h-[70px] text-ma-red">
           <use xlink:href="#leaves-1"></use>
